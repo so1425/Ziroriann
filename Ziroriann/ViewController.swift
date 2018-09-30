6b244dbc10c723fa29c7222254bb3e444f26df35
so1425 <rhc-myao425os@ezweb.ne.jp>
2018-09-30 17:10:44 +0900

    Initial Commit

---
+++ Ziroriann/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ziroriann
+//
+//  Created by so1425 on 2018/09/30.
+//  Copyright © 2018年 so1425. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ZiroriannTests/ZiroriannTests.swift
@@ -0,0 +1,34 @@
+//
+//  ZiroriannTests.swift
+//  ZiroriannTests
+//
+//  Created by so1425 on 2018/09/30.
+//  Copyright © 2018年 so1425. All rights reserved.
+//
+
+import XCTest
+@testable import Ziroriann
+
+class ZiroriannTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
